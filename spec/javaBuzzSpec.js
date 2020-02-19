3a40e0668d94d2cd2a870292ee8703514e6144fc
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-19 16:31:50 +0000

    Divisible by 5 passing Jasmine test

--- spec/javaBuzzSpec.js
+++ spec/javaBuzzSpec.js
@@ -2,18 +2,31 @@ describe("javaBuzz", function() {
 
   var javabuzz;
 
-  describe("knows when a number is", function() {
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
 
+  describe("knows when a number is", function() {
     it("divisible by 3", function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
 
   describe("knows when a number is NOT", function() {
     it("divisible by 3", function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
+
+  describe("knows when a number is", function() {
+    it("divisible by 5", function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe("knows when a number is NOT", function() {
+    it("divisible by 5", function() {
+      expect(javabuzz.isDivisibleByFive(4)).toBe(false);
     });
   });
 });
