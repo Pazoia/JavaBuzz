f8a549f703cbf41d9632e274f37cb9cf3e494235
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-19 16:08:15 +0000

    Divisible by 3 tests passing Jasmine

--- spec/javaBuzzSpec.js
+++ spec/javaBuzzSpec.js
@@ -6,7 +6,14 @@ describe("javaBuzz", function() {
 
     it("divisible by 3", function() {
       javabuzz = new Javabuzz();
-      expect(isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+  });
+
+  describe("knows when a number is NOT", function() {
+    it("divisible by 3", function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
   });
 });
