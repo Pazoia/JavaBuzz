836400571f9cb0eb0cce51254d1e20012c3b1e32
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-19 17:33:19 +0000

    Added instructions to log 1 to 100 in the console

--- src/javaBuzz.js
+++ src/javaBuzz.js
@@ -32,3 +32,12 @@ Javabuzz.prototype.display = function(number) {
 
   return number;
 };
+
+/*
+To display 1 to 100 in the console type the code below into the console.
+var javabuzz = new Javabuzz();
+
+for (var i = 1; i <= 100; i++) {
+  console.log(javabuzz.display(i));
+};
+*/
